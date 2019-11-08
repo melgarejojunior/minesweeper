c9d033d268fd6e9f05a5f4cdfda1181540363c6a
Junior Melgarejo (Safa) <juniormelgarejo@jera.com.br>
2019-11-07 21:16:19 -0400

    Base structure and assets done

--- app/src/main/java/com/juniormelgarejo/minesweeper/presentation/HomeActivity.kt
+++ app/src/main/java/com/juniormelgarejo/minesweeper/presentation/HomeActivity.kt
@@ -1,17 +1,36 @@
 package com.juniormelgarejo.minesweeper.presentation
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import com.juniormelgarejo.minesweeper.R
+import com.juniormelgarejo.minesweeper.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
 
+    private lateinit var binding: ActivityHomeBinding
     private lateinit var viewModel: HomeViewModel
     private var mineAdapter: MineAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+        viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        setupToolbar()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_home, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun setupToolbar() {
+        binding.toolbar.title = getString(R.string.app_name)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
     }
 }
